44b4c8f6468993dcbbdcfb110a5392485692ba4f
ChetanK <chetankalra11@gmail.com>
2016-09-18 03:31:21 +0530

    Signup Completion

--- LINKS/reg.php
+++ LINKS/reg.php
@@ -5,17 +5,22 @@ $Email= $_POST["Email"];
 $Age= $_POST["age"];
 $Date= $_POST["datevalid"];
 $Password= $_POST["password"];
+$Type= $_POST["type"];
 
-
-$con= mysqli_connect("localhost","root","","AME_SONNE");
-$query="insert into users(Name, Email, Age, Date, Password) VALUES('$username','$Email',$Age , '$Date' ,'$Password')";
+$con= mysqli_connect("localhost","root","","Art_Gallery");
+$query="insert into users(Name, Email, Age, Dob, Password, Type) VALUES('$username','$Email',$Age , '$Date' ,'$Password', $Type)";
 if(mysqli_query($con,$query))
 {
     header('Location: '."Login.html");
 }
 else{
     
-    echo mysqli_error($con);
+    //echo "Email Id already in use.";
+	//header('Location: '."Signup.html");
+	echo "<script type='text/javascript'>alert('Email ID already in use.');</script>";
+	
+	echo "<a href='Signup.html'><h3>Return</h3></a>";
+	
 }
 
 ?>
